9da6831133ceb0dae7754155d6054a4618ab6b22
semagarcia <jmanuelgarciagarcia@gmail.com>
2017-05-26 02:31:25 +0200

    Connecting to the database

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-var express = require('express');
-var app = express();
-var config = require('./config');
+const express = require('express');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const app = express();
+const config = require('./config');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
-app.listen(config.port, function () {
-  console.log(`Example app listening on http://localhost/${config.port}!`);
+app.get('/test', function (req, res) {
+    res.send({
+        status: 'ok',
+        timestamp: new Date()
+    });
+});
+
+mongoose.connect(config.db, (err, res) => {
+    if (err) {
+        return console.log(`Error connectin to the database: ${err}`)
+    }
+
+    app.listen(config.port, function () {
+        console.log(`Example app listening on http://localhost:${config.port}!`);
+    });
 });
