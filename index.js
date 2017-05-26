736e37d044dc647c1024124a322b5da7a16f4b8e
semagarcia <jmanuelgarciagarcia@gmail.com>
2017-05-26 03:07:13 +0200

    Fixed require improt

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
