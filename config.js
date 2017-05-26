d8647b8e1adbeab85dd47e49d8e25e7008d438e5
semagarcia <jmanuelgarciagarcia@gmail.com>
2017-05-26 02:38:24 +0200

    Fixed mongodb environment entry

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port:   process.env.PORT || 3000,
-    db:     process.env.MONGODB || 'mongodb://localhost:27017/kata-player',
+    db:     process.env.MONGODB_URI || 'mongodb://localhost:27017/kata-player',
     SECRET_TOKEN: 'claveeeee'
 }
