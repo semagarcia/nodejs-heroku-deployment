7a40173738c74c6c411a62ee591350e240cb156d
semagarcia <jmanuelgarciagarcia@gmail.com>
2017-05-26 03:02:19 +0200

    Added endpoint

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const app = express();
 const config = require('./config');
+
+const TestRowSchema = new Schema({
+    constName: { type: String },
+    access: { type: Date, default: Date.now() }
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/test', function (req, res) {
+app.get('/test', (req, res) => {
     res.send({
         status: 'ok',
         timestamp: new Date()
     });
+});
+
+app.get('/test-db', (req, res) => {
+    let RowEntry = mongoose.model('User', TestRowSchema);
+    let row = new RowEntry({ constName: 'Sema'  });
 });
 
 mongoose.connect(config.db, (err, res) => {
@@ -23,7 +36,7 @@ mongoose.connect(config.db, (err, res) => {
         return console.log(`Error connectin to the database: ${err}`)
     }
 
-    app.listen(config.port, function () {
+    app.listen(config.port, () => {
         console.log(`Example app listening on http://localhost:${config.port}!`);
     });
 });
