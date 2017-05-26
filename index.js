3c948ea216edfafb94c8619c4f65fc8cdf7456a4
semagarcia <jmanuelgarciagarcia@gmail.com>
2017-05-26 03:14:57 +0200

    Added endpoint to retrieve all elements

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const Schema = mongoose.Schema;
 
 const app = express();
 const config = require('./config');
-
 const TestRowSchema = new Schema({
     constName: { type: String },
     access: { type: Date, default: Date.now() }
 });
+const RowEntry = mongoose.model('RowEntries', TestRowSchema);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -26,11 +26,22 @@ app.get('/test', (req, res) => {
 });
 
 app.get('/test-db', (req, res) => {
-    let RowEntry = mongoose.model('RowEntries', TestRowSchema);
     let row = new RowEntry({ constName: 'Sema' });
     row.save((err) => {
         if(err) return res.status(500).send({ message: `Error inserting into DB: ${err}` });
         res.status(200).send({ result: 'ok' });
+    });
+});
+
+app.get('/db', (req, res) => {
+    RowEntry.find({}, function(err, entries) {
+        if(err) return res.status(500).send({ message: `Error inserting reading from DB: ${err}` });
+        
+        let rows = {};
+        entries.forEach(function(entry) {
+            rows[entry._id] = entry;
+        });
+        res.send(rows);  
     });
 });
 
